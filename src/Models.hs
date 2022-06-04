5f18ca1f0d929c82147d8af9eeab84a9ca47676c
Elman Hasa <elman.hasa@tesco.com>
2022-06-04 12:34:33 +0100

    Add message models

--- src/Models.hs
+++ src/Models.hs
@@ -15,3 +15,29 @@ data Block = Block {
 instance FromJSON Block 
 
 instance ToJSON Block
+
+data MessageType = NewPeer | NewBlock deriving (Show , Generic)
+
+data MessageData = NewPeerData {
+    peerAddr :: String
+} | NewBlockData {
+    block :: Block
+} deriving (Show , Generic)
+
+data Message = Message {
+    messageType :: MessageType,
+    msgTimeStamp :: String,
+    msgData :: MessageData
+} deriving (Show , Generic)
+
+instance FromJSON MessageType 
+
+instance FromJSON MessageData
+
+instance FromJSON Message 
+
+instance ToJSON MessageType 
+
+instance ToJSON MessageData
+
+instance ToJSON Message 
